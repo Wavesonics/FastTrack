9cd556ea95675c26911bde0a0a4e0fc93e6a9e59
Adam Brown <adamwbrown@gmail.com>
2020-09-28 16:24:55 -0500

    - Can now delete fasts

--- app/src/main/java/com/darkrockstudios/apps/fasttrack/screens/log/LogFragment.kt
+++ app/src/main/java/com/darkrockstudios/apps/fasttrack/screens/log/LogFragment.kt
@@ -9,9 +9,12 @@ import androidx.fragment.app.viewModels
 import com.darkrockstudios.apps.fasttrack.R
 import com.darkrockstudios.apps.fasttrack.data.database.AppDatabase
 import com.darkrockstudios.apps.fasttrack.data.database.FastEntry
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.mikepenz.fastadapter.FastAdapter
 import com.mikepenz.fastadapter.adapters.ItemAdapter
 import kotlinx.android.synthetic.main.log_fragment.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 
 class LogFragment: Fragment()
@@ -33,8 +36,24 @@ class LogFragment: Fragment()
 		super.onViewCreated(view, savedInstanceState)
 
 		log_entries.adapter = fastAdapter
+		fastAdapter.onClickListener = { _, _, item, _ ->
+			context?.let { ctx ->
+				MaterialAlertDialogBuilder(ctx)
+						.setTitle(R.string.confirm_delete_fast_title)
+						.setPositiveButton(R.string.confirm_delete_fast_positive) { _, _ -> deleteFast(item) }
+						.setNegativeButton(R.string.confirm_delete_fast_negative, null)
+						.show()
+			}
+
+			true
+		}
 
 		database.fastDao().loadAll().observe(viewLifecycleOwner, ::updateEntries)
+	}
+
+	private fun deleteFast(item: FastEntryItem)
+	{
+		GlobalScope.launch { database.fastDao().delete(item.fast) }
 	}
 
 	private fun updateEntries(entries: List<FastEntry>)
