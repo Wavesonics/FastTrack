53ae2f6ff86eea3eb59a630dfa7db179c924c541
Adam Brown <adamwbrown@gmail.com>
2020-10-03 16:59:48 -0600

    - Fixed AppBar theme

--- app/src/main/java/com/darkrockstudios/apps/fasttrack/MainActivity.kt
+++ app/src/main/java/com/darkrockstudios/apps/fasttrack/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity: AppCompatActivity()
 {
@@ -16,6 +17,8 @@ class MainActivity: AppCompatActivity()
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 		val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+		setSupportActionBar(appActionBar)
 
 		val navController = findNavController(R.id.nav_host_fragment)
 		// Passing each menu ID as a set of Ids because each
