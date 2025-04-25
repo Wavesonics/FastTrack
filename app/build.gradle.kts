4dffe2694c7c8b9ac8204f9ccce133ddca03c9d4
Adam Brown <adamwbrown@gmail.com>
2025-04-24 20:29:46 -0700

    Reproducible builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,126 +1,130 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.serialization)
-    alias(libs.plugins.ksp)
+	alias(libs.plugins.android.application)
+	alias(libs.plugins.kotlin.android)
+	alias(libs.plugins.kotlin.serialization)
+	alias(libs.plugins.ksp)
 }
 
 android {
-    namespace = "com.darkrockstudios.apps.fasttrack"
-    compileSdk = libs.versions.compileSdk.get().toInt()
+	namespace = "com.darkrockstudios.apps.fasttrack"
+	compileSdk = libs.versions.compileSdk.get().toInt()
 
-    defaultConfig {
-        applicationId = "com.darkrockstudios.apps.fasttrack"
-        minSdk = libs.versions.minSdk.get().toInt()
-        targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = libs.versions.versionCode.get().toInt()
-        versionName = libs.versions.versionName.get()
+	defaultConfig {
+		applicationId = "com.darkrockstudios.apps.fasttrack"
+		minSdk = libs.versions.minSdk.get().toInt()
+		targetSdk = libs.versions.targetSdk.get().toInt()
+		versionCode = libs.versions.versionCode.get().toInt()
+		versionName = libs.versions.versionName.get()
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-    signingConfigs {
-        create("release") {
-            storeFile = file(System.getenv("KEYSTORE_FILE") ?: "keystore.jks")
-            storePassword = System.getenv("KEYSTORE_PASSWORD") ?: ""
-            keyAlias = System.getenv("KEY_ALIAS") ?: ""
-            keyPassword = System.getenv("KEY_PASSWORD") ?: ""
-        }
-    }
-    buildFeatures {
-        viewBinding = true
-        buildConfig = true
-    }
-    buildTypes {
-        release {
-            isMinifyEnabled = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-            signingConfig = signingConfigs.getByName("release")
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.toVersion(libs.versions.javaVersion.get())
-        targetCompatibility = JavaVersion.toVersion(libs.versions.javaVersion.get())
-    }
-    kotlinOptions {
-        jvmTarget = libs.versions.javaVersion.get()
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
+		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+	}
+	signingConfigs {
+		create("release") {
+			storeFile = file(System.getenv("KEYSTORE_FILE") ?: "keystore.jks")
+			storePassword = System.getenv("KEYSTORE_PASSWORD") ?: ""
+			keyAlias = System.getenv("KEY_ALIAS") ?: ""
+			keyPassword = System.getenv("KEY_PASSWORD") ?: ""
+		}
+	}
+	buildFeatures {
+		viewBinding = true
+		buildConfig = true
+	}
+	buildTypes {
+		release {
+			isMinifyEnabled = true
+			proguardFiles(
+				getDefaultProguardFile("proguard-android-optimize.txt"),
+				"proguard-rules.pro"
+			)
+			signingConfig = signingConfigs.getByName("release")
+		}
+	}
+	compileOptions {
+		sourceCompatibility = JavaVersion.toVersion(libs.versions.javaVersion.get())
+		targetCompatibility = JavaVersion.toVersion(libs.versions.javaVersion.get())
+	}
+	kotlinOptions {
+		jvmTarget = libs.versions.javaVersion.get()
+	}
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_17
+		targetCompatibility = JavaVersion.VERSION_17
+	}
+	dependenciesInfo {
+		includeInApk = false
+		includeInBundle = false
+	}
 }
 
 dependencies {
-    implementation(libs.kotlinx.datetime)
-    implementation(libs.kotlinx.serialization.core)
-    implementation(libs.core.ktx)
-    implementation(libs.activity.ktx)
-    implementation(libs.collection.ktx)
-    implementation(libs.fragment.ktx)
-    implementation(libs.recyclerview)
-    implementation(libs.material)
+	implementation(libs.kotlinx.datetime)
+	implementation(libs.kotlinx.serialization.core)
+	implementation(libs.core.ktx)
+	implementation(libs.activity.ktx)
+	implementation(libs.collection.ktx)
+	implementation(libs.fragment.ktx)
+	implementation(libs.recyclerview)
+	implementation(libs.material)
 
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.constraintlayout)
+	implementation(libs.appcompat)
+	implementation(libs.material)
+	implementation(libs.constraintlayout)
 
-    implementation(libs.navigation.runtime.ktx)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
+	implementation(libs.navigation.runtime.ktx)
+	implementation(libs.androidx.navigation.fragment.ktx)
+	implementation(libs.androidx.navigation.ui.ktx)
 
-    implementation(libs.androidx.datastore.preferences)
+	implementation(libs.androidx.datastore.preferences)
 
-    implementation(libs.androidx.legacy.support.v4)
-    implementation(libs.androidx.lifecycle.extensions)
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.androidx.lifecycle.common.java8)
+	implementation(libs.androidx.legacy.support.v4)
+	implementation(libs.androidx.lifecycle.extensions)
+	implementation(libs.androidx.lifecycle.viewmodel.ktx)
+	implementation(libs.androidx.lifecycle.common.java8)
 
-    implementation(libs.kotlinx.coroutines.android)
+	implementation(libs.kotlinx.coroutines.android)
 
-    implementation(libs.androidx.room.runtime)
-    implementation(libs.androidx.cardview)
-    implementation(libs.androidx.lifecycle.livedata.ktx)
-    ksp(libs.androidx.room.compiler)
-    implementation(libs.androidx.room.ktx)
+	implementation(libs.androidx.room.runtime)
+	implementation(libs.androidx.cardview)
+	implementation(libs.androidx.lifecycle.livedata.ktx)
+	ksp(libs.androidx.room.compiler)
+	implementation(libs.androidx.room.ktx)
 
-    implementation(libs.napier)
+	implementation(libs.napier)
 
-    implementation(libs.fastadapter)
-    implementation(libs.fastadapter.extensions.binding)
+	implementation(libs.fastadapter)
+	implementation(libs.fastadapter.extensions.binding)
 
-    implementation(libs.glide)
-    ksp(libs.compiler)
+	implementation(libs.glide)
+	ksp(libs.compiler)
 
-    implementation(project.dependencies.platform(libs.koin.bom))
-    implementation(libs.koin.core)
-    implementation(libs.koin.android)
-    implementation(libs.koin.core.coroutines)
-    implementation(libs.koin.androidx.workmanager)
+	implementation(project.dependencies.platform(libs.koin.bom))
+	implementation(libs.koin.core)
+	implementation(libs.koin.android)
+	implementation(libs.koin.core.coroutines)
+	implementation(libs.koin.androidx.workmanager)
 
-    implementation(libs.customdatetimepicker)
+	implementation(libs.customdatetimepicker)
 
-    implementation(libs.satchel.core)
-    implementation(libs.satchel.storer.encrypted.file)
-    implementation(libs.satchel.serializer.base64.android)
+	implementation(libs.satchel.core)
+	implementation(libs.satchel.storer.encrypted.file)
+	implementation(libs.satchel.serializer.base64.android)
 
-    implementation(libs.materialabout)
+	implementation(libs.materialabout)
 
-    implementation(libs.konfetti)
+	implementation(libs.konfetti)
 
-    implementation(libs.core)
-    implementation(libs.ext.latex)
-    implementation(libs.ext.strikethrough)
-    implementation(libs.ext.tables)
-    implementation(libs.image)
-    implementation(libs.image.glide)
-    implementation(libs.linkify)
+	implementation(libs.core)
+	implementation(libs.ext.latex)
+	implementation(libs.ext.strikethrough)
+	implementation(libs.ext.tables)
+	implementation(libs.image)
+	implementation(libs.image.glide)
+	implementation(libs.linkify)
 
-    implementation(libs.appintro)
+	implementation(libs.appintro)
 
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+	testImplementation(libs.junit)
+	androidTestImplementation(libs.androidx.junit)
+	androidTestImplementation(libs.androidx.espresso.core)
 }
