fce5e3aadc297588c718fa8102249209a6e19877
Adam Brown <adamwbrown@gmail.com>
2025-10-26 17:25:16 -0700

    Don't allow manually adding a fast with an invalid end time

--- app/src/main/java/com/darkrockstudios/apps/fasttrack/screens/log/manualadd/ManualAddDialog.kt
+++ app/src/main/java/com/darkrockstudios/apps/fasttrack/screens/log/manualadd/ManualAddDialog.kt
@@ -1,13 +1,41 @@
 package com.darkrockstudios.apps.fasttrack.screens.log.manualadd
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.material3.rememberTimePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -21,6 +49,7 @@ import com.darkrockstudios.apps.fasttrack.screens.fasting.rememberDateTimePicker
 import com.darkrockstudios.apps.fasttrack.utils.PastAndTodaySelectableDates
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.atStartOfDayIn
+import kotlinx.datetime.toInstant
 import org.koin.compose.viewmodel.koinViewModel
 import kotlin.time.ExperimentalTime
 
@@ -191,6 +220,8 @@ fun ManualAddDialog(
 							if (showEndDateTimePicker) {
 								val dateTimePickerState = rememberDateTimePickerDialogState()
 								val initialEndInstant = uiState.end()
+								val minStartInstant =
+									uiState.selectedDateTime?.toInstant(TimeZone.currentSystemDefault())
 
 								DateTimePickerDialog(
 									onDismiss = { showEndDateTimePicker = false },
@@ -201,7 +232,8 @@ fun ManualAddDialog(
 									title = stringResource(R.string.manual_add_set_end_title),
 									finishButton = stringResource(id = R.string.manual_add_set_end_complete),
 									state = dateTimePickerState,
-									initialInstant = initialEndInstant
+									initialInstant = initialEndInstant,
+									minInstant = minStartInstant
 								)
 							}
 						}
